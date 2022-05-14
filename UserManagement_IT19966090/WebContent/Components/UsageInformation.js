9a926945606bb95a0f2d696b1e46f56290c15dd4
menaka809 <malithjayasekara2255721@gmail.com>
2022-05-14 14:18:24 +0530

    create save button handler inside controller

--- UserManagement_IT19966090/WebContent/Components/UsageInformation.js
+++ UserManagement_IT19966090/WebContent/Components/UsageInformation.js
@@ -4,3 +4,42 @@ $(document).ready(function()
  $("#alertSuccess").hide(); 
  $("#alertError").hide(); 
 });
+
+//save
+//add a click event handler for the Save button
+$(document).on("click", "#btnSave", function(event)
+{ 
+	
+//Clear alerts---------------------
+$("#alertSuccess").text(""); 
+$("#alertSuccess").hide(); 
+$("#alertError").text(""); 
+$("#alertError").hide(); 
+
+
+//Form validation-------------------
+var status = validateUsageForm(); 
+
+//If not valid
+if (status != true) 
+{ 
+$("#alertError").text(status); 
+$("#alertError").show(); 
+return; 
+} 
+
+
+//If valid------------------------
+var type = ($("#hideUsageInformationIDSave").val() == "") ? "POST" : "PUT"; 
+$.ajax( 
+{ 
+url : "UsageInformationAPI", 
+type : type, 
+data : $("#formUsageInformation").serialize(), 
+dataType : "text", 
+complete : function(response, status) 
+{ 
+onUsageInformationSaveComplete(response.responseText, status); 
+} 
+}); 
+});
